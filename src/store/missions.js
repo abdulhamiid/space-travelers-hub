bde111cbd770045bfadecae40c0bdd212543d5a0
Abdulhamid <adioabdulhamid@yahoo.com>
2022-07-18 22:25:58 +0100

    Create action and reducers to fetch data from API

--- src/store/missions.js
+++ src/store/missions.js
@@ -1,13 +1,43 @@
-import { createSlice } from '@reduxjs/toolkit';
+// import { createSlice } from '@reduxjs/toolkit';
 
-const slice = createSlice({
-  name: 'missions',
-  initialState: [],
-  reducers: {
-    checkStatus: () => 'Under construction',
-  },
+// const slice = createSlice({
+//   name: 'missions',
+//   initialState: [],
+//   reducers: {
+//     checkStatus: () => 'Under construction',
+//   },
+// });
+
+// export const { checkStatus } = slice.actions;
+
+// export default slice.reducer;
+
+import axios from 'axios';
+const allMission = []
+
+// Actions
+const FETCH_DATA = 'FETCH_DATA_FROM_API';
+
+// Action Creators
+const fetch = (mission) => ({
+  type: FETCH_DATA,
+  payload: mission,
 });
 
-export const { checkStatus } = slice.actions;
+// mission api
+const url = 'https://api.spacexdata.com/v3/missions';
 
-export default slice.reducer;
+export const fetchdata = () => async (dispatch) => {
+  const response = await axios.get(url);
+  const data = Object.entries(response.data).map((item) => {
+    const {
+      mission_id, mission_name, description
+    } = item;
+    return {
+      mission_id,
+      mission_name,
+      description,
+    };
+  });
+  dispatch(fetch(data));
+};
