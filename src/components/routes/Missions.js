c667e177c3bd042a24ed58eaee0c6a6afb1a8c3a
Abdulhamid <adioabdulhamid@yahoo.com>
2022-07-18 22:25:10 +0100

    Add layout for each mission

--- src/components/routes/Missions.js
+++ src/components/routes/Missions.js
@@ -3,6 +3,30 @@ import React from 'react';
 const Missions = () => {
   return (
     <main id="missions">
+      <tabel>
+        <thead>
+          <tr>
+            <td>Mission</td>
+            <td>Description</td>
+            <td>Status</td>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            array.map(item => (
+              <tr>
+                <td>{item.mission_name}</td>
+                <td>{item.description}</td>
+                <td>
+                  <button>NOT A MEMBER</button>
+                  <button>Join Mission</button>
+                </td>
+              </tr>
+            ))
+          }
+        </tbody>
+        
+      </tabel>
     </main>
   );
 };
