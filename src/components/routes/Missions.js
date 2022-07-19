1bed2be555487f212d8a7751f5b7f2f201ebf682
Abdulhamid <adioabdulhamid@yahoo.com>
2022-07-19 22:50:11 +0100

    Add head and body to the table element

--- src/components/routes/Missions.js
+++ src/components/routes/Missions.js
@@ -13,24 +13,28 @@ const Missions = () => {
   return (
     <main id="missions">
       <table className={styles.table}>
-        <tr>
-          <th>Mission</th>
-          <th>Description</th>
-          <th>Status</th>
-        </tr>
-        {state.map((item) => (
-          /* eslint-disable camelcase */
-          <tr key={item.mission_id} className={styles.tableRow}>
-            <td>{item.mission_name}</td>
-            <td>{item.description}</td>
-            <td>
-              <button type="button">NOT A MEMBER</button>
-            </td>
-            <td>
-              <button type="button">Join Mission</button>
-            </td>
+        <thead>
+          <tr>
+            <th>Mission</th>
+            <th>Description</th>
+            <th>Status</th>
           </tr>
-        ))}
+        </thead>
+        <tbody>
+          {state.map((item) => (
+            /* eslint-disable camelcase */
+            <tr key={item.mission_id} className={styles.tableRow}>
+              <td>{item.mission_name}</td>
+              <td>{item.description}</td>
+              <td>
+                <button type="button">NOT A MEMBER</button>
+              </td>
+              <td>
+                <button type="button">Join Mission</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </main>
   );
