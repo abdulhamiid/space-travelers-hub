da0122b47ef07e45e8a1cc532f877141d159bc0c
Abdulhamid <adioabdulhamid@yahoo.com>
2022-07-19 22:36:47 +0100

    Fix eslint errors

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
-import store from './store/configureStore';
+// import store from './store/configureStore';
+import store from './store/entities';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
