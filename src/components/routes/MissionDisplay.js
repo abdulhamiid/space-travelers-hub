94d0f218a815108dfdccd91e4af13ca28b248470
Abdulhamid <adioabdulhamid@yahoo.com>
2022-07-20 15:51:16 +0100

    Create component to render missions in the UI

---
+++ src/components/routes/MissionDisplay.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useSelector } from 'react-redux/es/exports';
+
+const MissionDisplay = () => {
+  const state = useSelector((state) => state.entities.missions);
+  const missions = state.filter((mission) => mission.reserved !== false);
+  console.log(state);
+  return (
+    <div className="missions">
+      <h2>My Missions</h2>
+      <ul>
+        {
+          missions.map((mission) => (
+            <li key={mission.mission_id}>{mission.mission_name}</li>
+          ))
+        }
+      </ul>
+    </div>
+  );
+};
+
+export default MissionDisplay;
