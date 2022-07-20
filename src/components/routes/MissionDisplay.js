10d79d04c7924c0d1d301f498546a84e1543d42b
Abdulhamid <adioabdulhamid@yahoo.com>
2022-07-20 17:28:36 +0100

    Remove console statement

--- src/components/routes/MissionDisplay.js
+++ src/components/routes/MissionDisplay.js
@@ -4,7 +4,6 @@ import { useSelector } from 'react-redux/es/exports';
 const MissionDisplay = () => {
   const state = useSelector((state) => state.entities.missions);
   const missions = state.filter((mission) => mission.reserved !== false);
-  console.log(state);
   return (
     <div id="missions">
       <h2>My Missions</h2>
