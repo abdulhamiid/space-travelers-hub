baa6989f1c46a4a4211b968e0b5b761e6fd9b620
Abdulhamid <adioabdulhamid@yahoo.com>
2022-07-20 15:52:04 +0100

    Import and add missions in the profile page

--- src/components/routes/MyProfile.js
+++ src/components/routes/MyProfile.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import MissionDisplay from './MissionDisplay';
 
 const MyProfile = () => (
-  <main id="my-profile" />
+  <main id="my-profile">
+    <MissionDisplay />
+    {/* you can replace the second component below with => My Rocket */}
+    <MissionDisplay />
+  </main>
 );
 
 export default MyProfile;
