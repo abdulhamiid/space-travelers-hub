19307c87d1fac48599ebb7f32b437a4179654c1e
Abdulhamid <adioabdulhamid@yahoo.com>
2022-07-20 09:32:15 +0100

    Import store from configureStore

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
-// import store from './store/configureStore';
-import store from './store/entities';
+import store from './store/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
