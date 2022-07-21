0304cd0f36d11afb13c751bdf215fa514a35af91
Debabrata Mandal <bapidebabrata@gmail.com>
2022-07-21 18:54:52 +0530

    Show reserved rockets list to my profile section

--- src/components/routes/MyProfile.js
+++ src/components/routes/MyProfile.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import MyMissions from '../views/MyMissons';
+import MyRockets from '../views/MyRockets';
+import styles from './MyProfile.module.css';
 
 const MyProfile = () => (
-  <main id="my-profile" />
+  <main id={styles.myProfile}>
+    <MyRockets />
+    <MyMissions />
+  </main>
 );
 
 export default MyProfile;
---
+++ src/components/views/MyMissons.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyMissions = () => {
+  return (
+    <div>
+      <h2>My Missions</h2>
+    </div>
+  );
+};
+
+export default MyMissions;
