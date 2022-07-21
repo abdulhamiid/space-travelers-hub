8932cc9af5c1e01e4d26976494c1e5d7c59af50a
Debabrata Mandal <bapidebabrata@gmail.com>
2022-07-21 19:01:02 +0530

    Fix linter errors

--- src/components/views/MyMissons.js
+++ src/components/views/MyMissons.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const MyMissions = () => {
-  return (
-    <div>
-      <h2>My Missions</h2>
-    </div>
-  );
-};
+const MyMissions = () => (
+  <div>
+    <h2>My Missions</h2>
+  </div>
+);
 
 export default MyMissions;
