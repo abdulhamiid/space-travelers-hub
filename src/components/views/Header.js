3d3d41e645198ed32190ed975334252ba2636834
abdulhamiid <adioabdulhamid@yahoo.com>
2024-11-23 15:02:15 +0100

    Fix linters errors

--- src/components/views/Header.js
+++ src/components/views/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../../logo.png';
 
@@ -13,19 +13,19 @@ const HamburgerMenu = () => {
     <header>
       <img src={logo} className="App-logo" alt="logo" />
       <h1>Space Travelers&apos; Hub</h1>
-    
-      <div className={`hamburger-menu ${isOpen ? "open" : ""}`}>
+
+      <div className={`hamburger-menu ${isOpen ? 'open' : ''}`}>
         <button className="hamburger-icon" onClick={toggleMenu}>
-          <span className="bar"></span>
-          <span className="bar"></span>
-          <span className="bar"></span>
+          <span className="bar" />
+          <span className="bar" />
+          <span className="bar" />
         </button>
         {isOpen && (
-            <nav className="menu">
-              <NavLink className={({ isActive }) => (isActive ? 'active-link' : undefined)} to="/">Rockets</NavLink>
-              <NavLink className={({ isActive }) => (isActive ? 'active-link' : undefined)} to="/missions">Missions</NavLink>
-              <NavLink className={({ isActive }) => (isActive ? 'active-link' : undefined)} to="/my-profile">My Profile</NavLink>
-            </nav>     
+        <nav className="menu">
+          <NavLink className={({ isActive }) => (isActive ? 'active-link' : undefined)} to="/">Rockets</NavLink>
+          <NavLink className={({ isActive }) => (isActive ? 'active-link' : undefined)} to="/missions">Missions</NavLink>
+          <NavLink className={({ isActive }) => (isActive ? 'active-link' : undefined)} to="/my-profile">My Profile</NavLink>
+        </nav>
         )}
       </div>
     </header>
